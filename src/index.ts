820379961eb0b77ec87ad19abb494fce2eba5234
NHeroOP <79276833+NHeroOP@users.noreply.github.com>
2026-09-20 00:00:51 +0530

    feat: integrate helmet middleware for enhanced security in HTTP server

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
 import { createServer } from "node:http";
 import { createSocketServer } from "@/setup/socket.js";
+import helmet from "helmet";
 
+const runHelmet = helmet();
 const PORT = Number(process.env.PORT ?? 5000);
-const httpServer = createServer();
+const httpServer = createServer(((req, res) => {
+  runHelmet(req, res, (err) => {
+    if (err) {
+      res.statusCode = 500;
+      res.end(
+        "Helmet failed for some unexpected reason. Was it configured correctly?",
+      );
+      return;
+    }
+  });
+}));
+
 
 createSocketServer(httpServer);
 
