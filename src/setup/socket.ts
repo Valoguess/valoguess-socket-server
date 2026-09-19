722df0ea54244c734a8030c60bd682a8db6187b4
NHeroOP <79276833+NHeroOP@users.noreply.github.com>
2026-09-20 00:23:23 +0530

    feat: implement token validation middleware for socket connections

--- src/setup/socket.ts
+++ src/setup/socket.ts
@@ -1,16 +1,54 @@
 import "dotenv/config";
 import { Server } from "node:http";
+import { jwtVerify, createRemoteJWKSet } from 'jose'
 import { Socket, Server as SocketServer } from "socket.io";
-
 import { registerHandlers } from "./register.js";
+import { AppError } from "@/shared/utils/error.js";
+
+const FrontendUrl = process.env.FRONTEND_URL || "http://localhost:3000";
+
+type SocketData = {
+  id: string;
+  name: string;
+}
+
+async function validateToken(token: string) {
+  try {
+    const JWKS = createRemoteJWKSet(
+      new URL(`${FrontendUrl}/api/auth/jwks`)
+    )
+    const { payload } = await jwtVerify(token, JWKS, {
+      issuer: `${FrontendUrl}`, 
+      audience: `${FrontendUrl}`,
+    })
+    return payload
+  } catch (error) {
+    console.error('Token validation failed:', error)
+    throw error
+  }
+}
 
 export function createSocketServer(server: Server) {
-  const io = new SocketServer(server, {
+  const io = new SocketServer<any,any,any,SocketData>(server, {
     cors: {
-      origin: ["http://localhost:3000", process.env.FRONTEND_URL!],
+      origin: [FrontendUrl],
       credentials: true,
     },
   });
+
+  io.use(async (socket, next) => {
+    const token = socket.handshake.auth.token;
+    if (!token) {
+      return next(new AppError("Authentication error: Token is missing"));
+    }
+    const payload = await validateToken(token)
+    socket.data = {
+      id: payload.id as string,
+      name: payload.name as string,
+    };
+
+    next()
+  })
 
   io.on("connect", (socket: Socket) => {
     console.log(`${socket.id} connected`);
